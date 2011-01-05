f4fe9d3dd5247a4c702a3da78e397033cea5926d
Artur <luke.upman@gmail.com>
2011-01-05 12:25:55 +0100

    sechste aenderung DomainFactory anlegen

--- SortedSetExample/src/at/jku/ce/Main.java
+++ SortedSetExample/src/at/jku/ce/Main.java
@@ -8,11 +8,11 @@ public class Main {
 	public static void main(String[] args) {
 		private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 		Set<DomainObject> objectSet = new HashSet<DomainObject>(); 
-		objectSet.add(new DomainObject("Helga","comment")); 
-		objectSet.add(new DomainObject("Walter","comment")); 
-		objectSet.add(new DomainObject("Hannah","comment")); 
-		objectSet.add(new DomainObject("Paul","comment")); 
-		objectSet.add(new DomainObject("Claudia","comment"));
+		objectSet.add(DomainFactory.createDomainObject(("Helga","comment")); 
+		objectSet.add(DomainFactory.createDomainObject(("Walter","comment")); 
+		objectSet.add(DomainFactory.createDomainObject(("Hannah","comment")); 
+		objectSet.add(DomainFactory.createDomainObject(("Paul","comment")); 
+		objectSet.add(DomainFactory.createDomainObject(("Claudia","comment"));
 		for (DomainObject domainObject : objectSet) {
 			LOGGER.debug(domainObject.getuuid());
 		}
