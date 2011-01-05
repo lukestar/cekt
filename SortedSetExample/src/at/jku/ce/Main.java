d92edbd794b622e783b658859859347b659e8a68
Clemens <Christoph@.(none)>
2011-01-05 10:47:03 +0100

    aenderung comment

--- SortedSetExample/src/at/jku/ce/Main.java
+++ SortedSetExample/src/at/jku/ce/Main.java
@@ -8,11 +8,11 @@ public class Main {
 	public static void main(String[] args) {
 		private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 		Set<DomainObject> objectSet = new HashSet<DomainObject>(); 
-		objectSet.add(new DomainObject("Helga")); 
-		objectSet.add(new DomainObject("Walter")); 
-		objectSet.add(new DomainObject("Hannah")); 
-		objectSet.add(new DomainObject("Paul")); 
-		objectSet.add(new DomainObject("Claudia"));
+		objectSet.add(new DomainObject("Helga","comment")); 
+		objectSet.add(new DomainObject("Walter","comment")); 
+		objectSet.add(new DomainObject("Hannah","comment")); 
+		objectSet.add(new DomainObject("Paul","comment")); 
+		objectSet.add(new DomainObject("Claudia","comment"));
 		for (DomainObject domainObject : objectSet) {
 			LOGGER.debug(domainObject.id);
 		}
