2c29a5b05a8112ea8a6bd8382427770035dc678c
Artur <luke.upman@gmail.com>
2011-01-05 10:23:12 +0100

    dritte aenderung

--- SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -6,15 +6,9 @@ public class DomainObject {
 	* Default constructor 
 	*/
 	
-	public String id = UUID.randomUUID().toString();
+	public String uuid = UUID.randomUUID().toString();
 	private String name;
 
-	
-<<<<<<< HEAD
-=======
-	public String Uuid;
-	private String name;	
->>>>>>> 1bc1647f629aa63c193d4f73e2140068a577ba77
 	
 	public DomainObject() {
 		super();
