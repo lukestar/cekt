add0604a6ca9b6bff226045e2f9de36e5f268ccd
Clemens <Christoph@.(none)>
2011-01-05 12:29:16 +0100

    aenderung protected

--- SortedSetExample/src/at/jku/ce/DomainFactory.java
+++ SortedSetExample/src/at/jku/ce/DomainFactory.java
@@ -1,7 +1,7 @@
 package at.jku.ce; 
 
 public class DomainFactory { 
-	public static DomainObject createDomainObject(String name, String comment) { 
+	protected static DomainObject createDomainObject(String name, String comment) { 
 		return new DomainObject(name, comment); 
 	} 
 }
