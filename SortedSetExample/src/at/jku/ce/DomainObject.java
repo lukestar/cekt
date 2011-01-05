f570e01ceedc54c5318be9cdcd3d2194e92f1251
Clemens <Christoph@.(none)>
2011-01-05 12:01:08 +0100

    aenderung comparable

--- SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -1,6 +1,6 @@
 package at.jku.ce;
 
-public class DomainObject {
+public class DomainObject implements Comparable<DomainObject>{
 
 	/** 
 	* Default constructor 
@@ -41,6 +41,12 @@ public class DomainObject {
 	
 	public void setName(final String name) { 
 		this.name = name; 
+	}
+
+	public int compareTo(DomainObject o) { 
+		DomainObject domainObj = (DomainObject) o; 
+		int domObjeComp = name.compareTo(domainObj.getName()); 
+		return ((domObjeComp == 0) ? uuid.compareTo(domainObj.getUuid()) : domObjeComp); 
 	}
 		
 }
