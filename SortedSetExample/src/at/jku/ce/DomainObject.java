070c391c5467a9ce65582c1717184ceef292831d
Artur <luke.upman@gmail.com>
2011-01-05 10:31:08 +0100

    vierte aenderung

--- SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -8,6 +8,7 @@ public class DomainObject {
 	
 	private String uuid = UUID.randomUUID().toString();
 	private String name;
+	private String comment;
 	
 	public DomainObject() {
 		super();
@@ -19,5 +20,13 @@ public class DomainObject {
 
 	public String getUuId() {
  		return id; 
+	}
+	
+	public String getComment() { 
+		return comment; 
+	} 
+	
+	public void setComment(String comment) { 
+		this.comment = comment; 
 	}
 }
