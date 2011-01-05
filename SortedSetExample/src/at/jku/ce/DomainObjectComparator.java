ca01b74414609f7d8a3104385bdd58c08018ea76
Barbara <Florian@.(none)>
2011-01-05 13:21:54 +0100

    siebte Aenderung comparetor

---
+++ SortedSetExample/src/at/jku/ce/DomainObjectComparator.java
@@ -0,0 +1,1 @@
+package at.jku.ce; import java.util.Comparator; public class DomainObjectComparator implements Comparator<DomainObject> { public int compare(DomainObject o1, DomainObject o2) {
--- SortedSetExample/src/at/jku/ce/Main.java
+++ SortedSetExample/src/at/jku/ce/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-		Set<DomainObject> objectSet = new HashSet<DomainObject>(); 
+		Set<DomainObject> objectSet = new TreeSet<DomainObject>(new DomainObjectComparator());
 		objectSet.add(new DomainObject("Helga","comment")); 
 		objectSet.add(new DomainObject("Walter","comment")); 
 		objectSet.add(new DomainObject("Hannah","comment")); 
