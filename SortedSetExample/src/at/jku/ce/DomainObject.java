0e277e0d7698591c538fdbae933d65daac01be67
Artur <luke.upman@gmail.com>
2011-01-05 10:07:05 +0100

    erste aenderungen domainobject.java: id-attribut und konsturuktor hinzugefuegt

--- SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -1,9 +1,14 @@
 package at.jku.ce;
 
 public class DomainObject {
-
+	
+	public String id; 
+	
 	public DomainObject() {
 		super();
 	}
-
+	
+	public DomainObject(String id) {
+		super(); this.id = id; 
+	}
 }
